ee2da9cf1084a4b737d05614afc9d826402e5869
Maria Carolina <demycar24@gmail.com>
2025-12-01 10:36:05 -0300

    feat: adicionando o banner

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+// lógica
+import Banner from './components/Banner.vue';
 import Header from './components/Header.vue';
 </script>
 
 <template>
+    <!-- as tags HTML -->
 <Header/>
+<Banner/>
 </template>
 
 <style></style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="banner">
+        <h1>Onde a paisagem vira 
+            <span>inspiração</span> .</h1>
+            <p>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</p>
+    </section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
