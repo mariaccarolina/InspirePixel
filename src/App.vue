eb56e2b7ba9ed0a0dd8bbb9d9de9fcc328da7420
Maria Carolina <demycar24@gmail.com>
2025-12-03 10:29:48 -0300

    feat: Adiciona os card no inspire

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 // lógica
 import Banner from './components/Banner.vue';
 import Header from './components/Header.vue';
+import Inspire from './components/Inspire.vue';
 </script>
 
 <template>
     <!-- as tags HTML -->
 <Header/>
 <Banner/>
+<Inspire/>
 </template>
 
 <style></style>
